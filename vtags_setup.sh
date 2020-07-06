15b3a5a8adf7bbcbfc8a7bd68513c31da5032d57
Heaton15 <Heaton15@gmail.com>
2020-07-06 22:15:19 +0000

    Fixed path

--- vtags_setup.sh
+++ vtags_setup.sh
@@ -3,6 +3,7 @@ if [[ ${USER} == "tim.heaton" || ${USER} == "heaton.49" ]]; then
     curl -o $HOME/vtags.tar.gz https://www.vim.org/scripts/download_script.php?src_id=26794
     cd $HOME
     tar -xvzf vtags.tar.gz
+    rm vtags.tar.gz
     mv vtags-3.01 .vtags-3.01
     popd 
 fi
