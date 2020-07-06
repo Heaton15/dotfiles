7af49a150ba0bff5c09bf43a051f97f7df2adb83
Heaton15 <Heaton15@gmail.com>
2020-07-06 22:20:16 +0000

    Added version variable to vtags

--- vtags_setup.sh
+++ vtags_setup.sh
@@ -1,9 +1,11 @@
+VERSION='vtags-3.01'
+
 if [[ ${USER} == "tim.heaton" || ${USER} == "heaton.49" ]]; then
     pushd .
-    curl -o $HOME/vtags.tar.gz https://www.vim.org/scripts/download_script.php?src_id=26794
+    curl -o $HOME/$VERSION.tar.gz https://www.vim.org/scripts/download_script.php?src_id=26794
     cd $HOME
-    tar -xvzf vtags.tar.gz
-    rm vtags.tar.gz
+    tar -xvzf ${VERSION}.tar.gz
+    rm ${VERSION}.tar.gz
     mv vtags-3.01 .vtags-3.01
     popd 
 fi
