8ffb7aa4e336fe07128fab58f8dd806955ce44ee
Heaton15 <Heaton15@gmail.com>
2020-07-21 18:04:26 +0000

    Added metals setup script for YCM

---
+++ metals_setup.sh
@@ -0,0 +1,9 @@
+ coursier bootstrap \
+     --java-opt -Xss4m \
+     --java-opt -Xms100m \
+     --java-opt -Dmetals.client=YouCompleteMe \
+     org.scalameta:metals_2.12:0.9.2 \
+     -r bintray:scalacenter/releases \
+     -r sonatype:snapshots \
+     -o $HOME/.linuxbrew/bin/metals-vim
+
