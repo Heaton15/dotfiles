45bea46dc5ab0e1155f3f2f14d70ca33afb80576
Tim H <Heaton15@gmail.com>
2021-07-12 15:04:49 +0000

    new script

--- scripts/RHEL7_brew.sh
+++ scripts/RHEL7_brew.sh
@@ -1,12 +1,12 @@
 # Installs the linuxbrew tools
-source /opt/rh/rh-git29/enable
-source /opt/rh/devtoolset-8/enable
-export HOMEBREW_NO_ENV_FILTERING=1
-export HOMEBREW_CURLRC=1
+#source /opt/rh/rh-git29/enable
+source /opt/rh/devtoolset-10/enable
+#export HOMEBREW_NO_ENV_FILTERING=1
+#export HOMEBREW_CURLRC=1
 
 #echo "insecure" >> ~/.curlrc
-echo "export HOMEBREW_NO_ENV_FILTERING=1" >> ~/.bashrc
-echo "export HOMEBREW_CURLRC=1" >> ~/.bashrc
+#echo "export HOMEBREW_NO_ENV_FILTERING=1" >> ~/.bashrc
+#echo "export HOMEBREW_CURLRC=1" >> ~/.bashrc
 echo "export PATH=\$HOME/.linuxbrew/bin:\$PATH" >> ~/.bashrc
 echo "export PATH=\$HOME/.linuxbrew/sbin:\$PATH" >> ~/.bashrc
 
@@ -15,6 +15,7 @@ echo "export PATH=\$HOME/.linuxbrew/sbin:\$PATH" >> ~/.bashrc
 #Update and upgrade in case anything is left over 
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export PATH="$HOME/.linuxbrew/sbin:$PATH"
+export PATH="/home/heaton.49/.linuxbrew/bin/git:$PATH"
 brew update
 brew upgrade
 # yes you have to do this
