dec4c74f4a72f85632819f8b9dfcd479c8ad0cdb
Tim H <heaton15@gmail.com>
2022-06-07 22:41:54 -0400

    1. Adding metals install script

--- scripts/metals_setup.sh
+++ scripts/metals_setup.sh
@@ -1,10 +1,11 @@
- coursier bootstrap \
-     --java-opt -Xss4m \
-     --java-opt -Xms100m \
-     --java-opt -Dmetals.client=YouCompleteMe \
-     --java-opt -Dmetals.extensions=true \
-     org.scalameta:metals_2.12:0.9.2 \
-     -r bintray:scalacenter/releases \
-     -r sonatype:snapshots \
-     -o $HOME/.linuxbrew/bin/metals-vim -f
-
+ # Make sure to use coursier v1.1.0-M9 or newer.
+curl -L -o coursier https://git.io/coursier-cli
+chmod +x coursier
+./coursier bootstrap \
+  --java-opt -Xss4m \
+  --java-opt -Xms100m \
+  --java-opt -Dmetals.client=vim-lsc \
+  org.scalameta:metals_2.13:0.11.6 \
+  -r bintray:scalacenter/releases \
+  -r sonatype:snapshots \
+  -o $HOME/.local/bin/metals-vim -f
