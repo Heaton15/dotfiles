7a7214f6d6b3649b3ef450af85b029fc3e7b2324
Tim H <heaton15@gmail.com>
2022-06-07 23:31:20 -0400

    1. Placing coursier install in local bin

--- scripts/metals_setup.sh
+++ scripts/metals_setup.sh
@@ -1,11 +1,13 @@
- # Make sure to use coursier v1.1.0-M9 or newer.
-curl -L -o coursier https://git.io/coursier-cli
-chmod +x coursier
-./coursier bootstrap \
+# Make sure to use coursier v1.1.0-M9 or newer.
+
+IPATH=$HOME/.local/bin
+curl -L -o $IPATH/coursier https://git.io/coursier-cli
+chmod +x $IPATH/coursier
+$IPATH/coursier bootstrap \
   --java-opt -Xss4m \
   --java-opt -Xms100m \
   --java-opt -Dmetals.client=vim-lsc \
   org.scalameta:metals_2.13:0.11.6 \
   -r bintray:scalacenter/releases \
   -r sonatype:snapshots \
-  -o $HOME/.local/bin/metals-vim -f
+  -o $IPATH/metals-vim -f
