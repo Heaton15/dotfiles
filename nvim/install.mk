ba8697eb90e2a09a8a0dca891a8f45f6863090f8
Tim H <Heaton15@gmail.com>
2022-09-15 07:45:01 -0700

    Added nvim paths for make system

--- nvim/install.mk
+++ nvim/install.mk
@@ -2,6 +2,7 @@
 ALL_TARGETS += nvim
 
 nvim:
-	./install_file.sh nvim/init.lua   ~/.config/nvim/init.lua
-	./install_dir.sh  nvim/lua        ~/.config/nvim/lua
-	./install_dir.sh  nvim/lua/after ~/.config/nvim/after
+	./install_file.sh nvim/init.lua   	     ~/.config/nvim/init.lua
+	./install_dir.sh  nvim/lua        	     ~/.config/nvim/lua
+	./install_dir.sh  nvim/lua/config        ~/.config/nvim/lua/config
+	./install_dir.sh  nvim/after/syntax      ~/.config/nvim/after/syntax
