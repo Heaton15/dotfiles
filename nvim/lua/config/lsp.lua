b96e40103b8f9da7c3b1dfc67e082004d57eeb66
Tim H <Heaton15@gmail.com>
2022-10-19 18:40:04 -0700

    Updated lsp config to not use update_capabilities anymore

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -4,8 +4,8 @@ require("nvim-lsp-installer").setup {}
 local lspconfig = require("lspconfig")
 
 -- Adds cmp as a capability to the lsp autocompletion
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
