8e0707e995aeb77bc3617dc1d7d271a709aeba6d
Tim H <Heaton15@gmail.com>
2024-07-23 11:43:26 -0700

    dotfiles: Using carbonfox once again

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,41 @@
 return {
-    {
-        "talha-akram/noctis.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme noctis_obscuro")
-            vim.cmd([[highlight LineNr guibg=NONE]])
-        end,
+    --{
+    --    "talha-akram/noctis.nvim",
+    --    lazy = false,
+    --    priority = 1000,
+    --    config = function()
+    --        vim.cmd("colorscheme noctis_obscuro")
+    --        vim.cmd([[highlight LineNr guibg=NONE]])
+    --    end,
+    --},
+    
+    { 
+       "EdenEast/nightfox.nvim",
+         lazy = false,
+         priority = 1000,
+         config = function()
+           vim.cmd("colorscheme carbonfox")
+           vim.cmd([[highlight LineNr guibg=NONE]])
+         end,
     },
+
+    --{
+    --   "folke/tokyonight.nvim",
+    --   lazy = false,
+    --   priority = 1000,
+    --   opts = {},
+    --   config = function()
+    --       vim.cmd("colorscheme tokyonight-moon")
+    --   end,
+    --},
+    
+    --{ 
+    --    "bluz71/vim-nightfly-colors",
+    --    name = "nightfly",
+    --    lazy = false,
+    --    priority = 1000,
+    --    config = function()
+    --        vim.cmd("colorscheme nightfly")
+    --    end,
+    --},
 }
