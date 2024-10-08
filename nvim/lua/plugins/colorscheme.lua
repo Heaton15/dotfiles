84d1b3e215bdc68e7cdd51a67bf9f31beff37b48
Tim <heaton15@gmail.com>
2024-10-08 01:28:26 -0400

    colorscheme updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,33 +9,24 @@ return {
     --    end,
     --},
     
-    { 
-       "EdenEast/nightfox.nvim",
-         lazy = false,
-         priority = 1000,
-         config = function()
-           vim.cmd("colorscheme carbonfox")
-           vim.cmd([[highlight LineNr guibg=NONE]])
-         end,
-    },
-
-    --{
-    --   "folke/tokyonight.nvim",
-    --   lazy = false,
-    --   priority = 1000,
-    --   opts = {},
-    --   config = function()
-    --       vim.cmd("colorscheme tokyonight-moon")
-    --   end,
-    --},
-    
     --{ 
-    --    "bluz71/vim-nightfly-colors",
-    --    name = "nightfly",
-    --    lazy = false,
-    --    priority = 1000,
-    --    config = function()
-    --        vim.cmd("colorscheme nightfly")
-    --    end,
+    --   "EdenEast/nightfox.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --       vim.cmd("colorscheme carbonfox")
+    --       vim.cmd([[highlight LineNr guibg=NONE]])
+    --     end,
     --},
+
+    { 
+      'keiyakeiya/PapilioDehaanii.vim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd("colorscheme papilio_dehaanii")
+        vim.cmd([[highlight LineNr guibg=NONE]])
+      end,
+
+    }
 }
