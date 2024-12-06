179f027aab97d3dbe35699d88b098811b9322e9a
Tim H <heaton15@gmail.com>
2024-12-05 19:10:57 -0500

    Formatting Removing venn. Was a fun plugin to mess around with, but not commonly used.

--- nvim/lua/config/vtags.lua
+++ nvim/lua/config/vtags.lua
@@ -1,6 +1,6 @@
 vim.notify = require("notify")
 local vtags_version = "https://www.vim.org/scripts/download_script.php?src_id=26794" -- 3.01
-local vtags_3p10 = "https://www.vim.org/scripts/download_script.php?src_id=27765" --3.10
+local vtags_3p10 = "https://www.vim.org/scripts/download_script.php?src_id=27765"    --3.10
 local version = "3.01"
 local dest = ".vtags-" .. version
 
@@ -8,20 +8,20 @@ local dest = ".vtags-" .. version
 
 local vtags_path = "$HOME/" .. dest .. "/vtags-" .. version .. "/vtags_vim_api.vim"
 if vim.fn.filereadable(vim.fn.expand(vtags_path)) == 0 then
-	vim.notify("No Vtags Detected. ---Installing---", "INFO")
-	os.execute("curl -o ~/.vtags.tar.gz " .. vtags_version)
-	os.execute("mkdir ~/" .. dest .. " && tar -xvzf ~/.vtags.tar.gz -C ~/" .. dest)
+    vim.notify("No Vtags Detected. ---Installing---", "INFO")
+    os.execute("curl -o ~/.vtags.tar.gz " .. vtags_version)
+    os.execute("mkdir ~/" .. dest .. " && tar -xvzf ~/.vtags.tar.gz -C ~/" .. dest)
 end
 
 -- Create vtags keybinds. This is pretty clanky compared to other plugins
 local vlog_group = vim.api.nvim_create_augroup("verilog_key_bindings", {})
 
 function vlog_bind(cmd, augroup)
-	vim.api.nvim_create_autocmd("Filetype", {
-		pattern = "verilog_systemverilog",
-		command = cmd,
-		group = augroup,
-	})
+    vim.api.nvim_create_autocmd("Filetype", {
+        pattern = "verilog_systemverilog",
+        command = cmd,
+        group = augroup,
+    })
 end
 
 vlog_bind("map <leader>a <Plug>VerilogEmacsAutoAdd :redraw! <CR>", vlog_group)
@@ -29,16 +29,16 @@ vlog_bind("map <leader><leader>a <Plug>VerilogEmacsAutoAdd", vlog_group)
 vlog_bind("map <leader>d <Plug>VerilogEmacsAutoDelete :redraw! <CR>", vlog_group)
 vlog_bind("map <leader><leader>d <Plug>VerilogEmacsAutoDelete", vlog_group)
 vlog_bind(
-	"nnoremap <buffer> <leader>v : py3 try_show_frame() <CR> <C-w>h <CR> : set filetype=verilog_systemverilog <CR>",
-	vlog_group
+    "nnoremap <buffer> <leader>v : py3 try_show_frame() <CR> <C-w>h <CR> : set filetype=verilog_systemverilog <CR>",
+    vlog_group
 )
 vlog_bind(
-	"nnoremap <buffer> <leader>i : py3 try_go_into_submodule() <CR> : py try_show_frame() <CR> : py try_print_module_trace() <CR>",
-	vlog_group
+    "nnoremap <buffer> <leader>i : py3 try_go_into_submodule() <CR> : py try_show_frame() <CR> : py try_print_module_trace() <CR>",
+    vlog_group
 )
 vlog_bind(
-	"nnoremap <buffer> <leader>u : py3 try_go_upper_module() <CR> : py try_show_frame() <CR> : py try_print_module_trace() <CR>",
-	vlog_group
+    "nnoremap <buffer> <leader>u : py3 try_go_upper_module() <CR> : py try_show_frame() <CR> : py try_print_module_trace() <CR>",
+    vlog_group
 )
 vlog_bind("nnoremap <buffer> <leader>mt      : py3 try_print_module_trace()        <CR>", vlog_group)
 vlog_bind("nnoremap <buffer> <leader>ct      : py3 clear_trace()                   <CR>", vlog_group)
--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,127 +1,102 @@
 return {
-	{
-		"jbyuki/venn.nvim",
-		config = function()
-			function _G.Toggle_venn()
-				local venn_enabled = vim.inspect(vim.b.venn_enabled)
-				if venn_enabled == "nil" then
-					vim.b.venn_enabled = true
-					vim.cmd([[setlocal ve=all]])
-					-- draw a line on HJKL keystokes
-					vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
-					vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
-					vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
-					vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
-					-- draw a box by pressing "f" with visual selection
-					vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
-					vim.api.nvim_buf_set_keymap(0, "v", "o", ":VBoxO<CR>", { noremap = true })
-				else
-					vim.cmd([[setlocal ve=]])
-					vim.cmd([[mapclear <buffer>]])
-					vim.b.venn_enabled = nil
-				end
-			end
-			vim.keymap.set("n", "<leader>ve", ":lua Toggle_venn()<CR>", { noremap = true })
-		end,
-	},
-	{
-		"sustech-data/wildfire.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = {},
-	},
-	{
-		"dhruvasagar/vim-table-mode",
-		event = "VeryLazy",
-		config = function()
-			local ft = vim.bo.filetype
-			if ft == "rst" then
-				vim.g.table_mode_corner_corner = [[+]]
-				vim.g.table_mode_header_fillchar = [[=]]
-			elseif ft == "markdown" then
-				vim.cmd("let b:table_mode_corner = '|'")
-			end
-		end,
-	},
+    {
+        "sustech-data/wildfire.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {},
+    },
+    {
+        "dhruvasagar/vim-table-mode",
+        event = "VeryLazy",
+        config = function()
+            local ft = vim.bo.filetype
+            if ft == "rst" then
+                vim.g.table_mode_corner_corner = [[+]]
+                vim.g.table_mode_header_fillchar = [[=]]
+            elseif ft == "markdown" then
+                vim.cmd("let b:table_mode_corner = '|'")
+            end
+        end,
+    },
 
-	{ "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+    { "L3MON4D3/LuaSnip",                    version = "v2.*",  build = "make install_jsregexp" },
 
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = { "L3MON4D3/LuaSnip" },
-		opts = function()
-			local has_words_before = function()
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = { "L3MON4D3/LuaSnip" },
+        opts = function()
+            local has_words_before = function()
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0
+                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
-			local feedkey = function(key, mode)
-				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-			end
+            local feedkey = function(key, mode)
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+            end
 
-			vim.cmd([[set completeopt=menu,menuone,noselect]])
+            vim.cmd([[set completeopt=menu,menuone,noselect]])
 
-			local cmp = require("cmp")
-			local select_opts = { behavior = cmp.SelectBehavior.Select }
-			return {
-				-- In some cases, the selection is auto filled in even though I
-				-- haven't called cmp.select_next_item(). This prevents the autofill
-				-- from occurring.
-				preselect = cmp.PreselectMode.None,
-				snippet = {
-					expand = function(args)
-						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<C-p>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+            local cmp = require("cmp")
+            local select_opts = { behavior = cmp.SelectBehavior.Select }
+            return {
+                -- In some cases, the selection is auto filled in even though I
+                -- haven't called cmp.select_next_item(). This prevents the autofill
+                -- from occurring.
+                preselect = cmp.PreselectMode.None,
+                snippet = {
+                    expand = function(args)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<C-p>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
 
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					--['<C-o>'] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					-- trying this without select = true because we can't <CR> in insert
-					-- mode without autocompleting junk. Maybe there is a better way,
-					-- but idk
-					["<CR>"] = cmp.mapping.confirm(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lsp_signature_help" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			}
-		end,
-		config = function(_, opts)
-			require("cmp").setup(opts)
-		end,
-	},
-	{ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" },
-	{ "hrsh7th/cmp-buffer", after = "nvim-cmp" },
-	{ "hrsh7th/cmp-path", after = "nvim-cmp" },
-	{ "hrsh7th/cmp-cmdline", after = "nvim-cmp" },
-	{ "hrsh7th/cmp-nvim-lsp-signature-help", after = "nvim-cmp" },
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    --['<C-o>'] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    -- trying this without select = true because we can't <CR> in insert
+                    -- mode without autocompleting junk. Maybe there is a better way,
+                    -- but idk
+                    ["<CR>"] = cmp.mapping.confirm(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lsp_signature_help" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            }
+        end,
+        config = function(_, opts)
+            require("cmp").setup(opts)
+        end,
+    },
+    { "hrsh7th/cmp-nvim-lsp",                after = "nvim-cmp" },
+    { "hrsh7th/cmp-buffer",                  after = "nvim-cmp" },
+    { "hrsh7th/cmp-path",                    after = "nvim-cmp" },
+    { "hrsh7th/cmp-cmdline",                 after = "nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp-signature-help", after = "nvim-cmp" },
 }
