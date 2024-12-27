5c7afec7cff73360b410882fe918b291fae43f65
Tim H <heaton15@gmail.com>
2024-12-27 11:26:07 -0500

    lint

--- ghostty/install.mk
+++ ghostty/install.mk
@@ -2,5 +2,5 @@
 ALL_TARGETS += ghostty
 
 ghostty:
-	./install_file.sh ghostty/config ~/.config/ghostty/config
-	./install_dir.sh ghostty/themes ~/.config/ghostty/themes
+	./install_file.sh ghostty/config  ~/.config/ghostty/config
+	./install_dir.sh  ghostty/themes   ~/.config/ghostty/themes
