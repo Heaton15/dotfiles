89174aefe5b95dd629ea1dcd3ba021afadeed397
Tim H <heaton15@gmail.com>
2025-04-01 11:27:18 -0400

    Move "coding plugins" to new locations

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -97,5 +97,45 @@ return {
             vim.g.loaded_netrwPlugin = 1
             require("nvim-tree").setup(opts)
         end,
-    }
+    },
+    {
+        'saghen/blink.cmp',
+        lazy = false,
+        dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+        version = '*',
+        opts = {
+            snippets = {
+                expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+                active = function(filter)
+                    if filter and filter.direction then
+                        return require('luasnip').jumpable(filter.direction)
+                    end
+                    return require('luasnip').in_snippet()
+                end,
+                jump = function(direction) require('luasnip').jump(direction) end,
+            },
+            keymap = {
+                ["<C-n>"] = { "select_next", "snippet_forward" },
+                ["<C-p>"] = { "select_prev", "snippet_backward" },
+            },
+            completion = {
+                list = {
+                    -- auto completes the LSP options
+                    selection = { auto_insert = true }
+                },
+                menu = {
+                    max_height = 30,
+                },
+            },
+
+            cmdline = {
+                sources = {},
+            },
+
+            sources = {
+                default = { 'lsp', 'path', 'buffer' },
+            },
+        },
+        opts_extend = { "sources.default" }
+    },
 }
--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -1,4 +1,35 @@
 return {
-	"nvim-lua/plenary.nvim",
-	lazy = true,
+    {
+        "nvim-lua/plenary.nvim",
+        lazy = true,
+    },
+    {
+        "dhruvasagar/vim-table-mode",
+        event = "VeryLazy",
+        config = function()
+            local ft = vim.bo.filetype
+            if ft == "rst" then
+                vim.g.table_mode_corner_corner = [[+]]
+                vim.g.table_mode_header_fillchar = [[=]]
+            elseif ft == "markdown" then
+                vim.cmd("let b:table_mode_corner = '|'")
+            end
+        end,
+    },
+
+    -- Makes creating tables for md, rst, etc a lot easier
+    {
+        "dhruvasagar/vim-table-mode",
+        event = "VeryLazy",
+        config = function()
+            local ft = vim.bo.filetype
+            if ft == "rst" then
+                vim.g.table_mode_corner_corner = [[+]]
+                vim.g.table_mode_header_fillchar = [[=]]
+            elseif ft == "markdown" then
+                vim.cmd("let b:table_mode_corner = '|'")
+            end
+        end,
+    },
+
 }
