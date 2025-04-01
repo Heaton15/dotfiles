1f4de1c0250ed71046375f0a02e4749ba77ab6e1
Tim H <heaton15@gmail.com>
2025-03-31 21:51:32 -0400

    Move most of the neovim config to the config/ folder

---
+++ nvim/lua/config/keybinds.lua
@@ -0,0 +1,51 @@
+-- telescope
+local initlua = vim.fn.expand("$HOME/.config/nvim")
+
+vim.keymap.set("n", "<leader>h", require("telescope.builtin").help_tags)
+vim.keymap.set("n", "<leader>ev", ":Telescope find_files follow=true cwd=" .. initlua .. " <CR>")
+vim.keymap.set("v", "<leader>y", "\"*y")
+vim.keymap.set("n", "<leader>eb", ":edit $HOME/.bashrc <CR>")
+vim.keymap.set("n", "<leader>q", ":qa! <CR>")
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>ws", "<C-w>s")
+vim.keymap.set("n", "<leader>w/", "<C-w>v")
+
+-- Tab movement in nvim
+vim.keymap.set("n", "tn", ":tabnew <CR>")
+vim.keymap.set("n", "tc", ":tabclose <CR>")
+vim.keymap.set("n", "tN", ":tab sball <CR>")
+vim.keymap.set("n", "ts", ":tab split <CR>")
+
+-- Move between tabs with leader + num
+for i = 1, 9 do
+    local key = "<leader>" .. i
+    vim.keymap.set("n", key, i .. "gt")
+end
+
+vim.keymap.set("n", "<F5>", [[ :set norelativenumber! <CR> :set nonumber! <CR>]])
+
+-- Window control in neovim
+vim.keymap.set("n", "<leader>l", ":redraw <CR>", { silent = true })
+vim.keymap.set("n", "<leader>ww", "<C-w>= <CR>", { silent = true })
+
+-- tmux / vimux config
+vim.keymap.set("n", "<leader>vp", ":VimuxPromptCommand<CR>")
+vim.keymap.set("n", "<leader>vc", ":VimuxCloseRunner<CR>")
+vim.keymap.set("n", "<leader>vo", ":VimuxOpenRunner<CR>")
+vim.keymap.set("n", "<leader>vl", ":VimuxRunLastCommand<CR>")
+vim.keymap.set("n", "<leader>vi", ":VimuxInspectRunner<CR>")
+
+-- vim buffer navigation
+vim.g.tmux_navigator_no_mappings = 1
+vim.keymap.set("n", "<leader>wh", ":TmuxNavigateLeft <CR>", { silent = true })
+vim.keymap.set("n", "<leader>wj", ":TmuxNavigateDown <CR>", { silent = true })
+vim.keymap.set("n", "<leader>wk", ":TmuxNavigateUp <CR>", { silent = true })
+vim.keymap.set("n", "<leader>wl", ":TmuxNavigateRight <CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>ctl", function()
+    local toggle = not vim.diagnostic.config().virtual_lines
+    vim.diagnostic.config({ virtual_lines = toggle })
+end, { desc = "Toggles diagnostic virtual lines" })
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,75 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+
+
+vim.opt.rtp:prepend(lazypath)
+
+-- nvim options
+local indent = 4
+
+vim.opt.visualbell = true
+vim.opt.termguicolors = true
+vim.opt.ttimeoutlen = 5
+vim.opt.splitright = true
+vim.opt.foldmethod = "manual"
+vim.opt.virtualedit = "all"
+vim.opt.mouse = "a"
+vim.wo.wrap = true
+vim.opt.modelines = 0
+vim.opt.expandtab = true
+vim.opt.tabstop = indent
+vim.opt.softtabstop = indent
+vim.opt.shiftwidth = indent
+vim.opt.list = true
+vim.opt.listchars = {
+    -- Place a '#' in the last column when 'wrap' is off and the line continues beyond the right of the screen
+    extends = "#",
+    -- Place a '#' in the first column when 'wrap' is off and there is text preceding the character visible in the
+    -- first column
+    precedes = "#",
+    -- Show non-breaking space characters
+    nbsp = "¬",
+    -- Show tabs
+    tab = ".→",
+    -- Show trailing spaces
+    trail = "◊",
+}
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.bo.formatoptions = "crot"
+
+-- Search settings
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt_global.shortmess:remove("F")
+
+-- Keybinds Updates
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[ ]]
+
+vim.diagnostic.config({ virtual_lines = true })
+vim.o.winborder = 'rounded'
+vim.cmd([[filetype plugin indent on]])
+vim.cmd([[set noea "set equalalways]])
+vim.cmd([[set backspace=indent,eol,start]])
+vim.cmd("set rtp+=~/dotfiles/myhelp/")
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+})
