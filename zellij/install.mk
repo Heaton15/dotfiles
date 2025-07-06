67fa931bda9b032c1a1c370651d9bb09922263a5
Tim H <Heaton15@gmail.com>
2025-07-06 19:10:33 -0400

    Even if we don't set the theme_dir option for zellij, you can still add the themes options in the zellij/themes/ directory and it will pick them up

--- zellij/install.mk
+++ zellij/install.mk
@@ -2,5 +2,6 @@
 ALL_TARGETS += zellij-cfg
 
 zellij-cfg:
-	./install_file.sh zellij/config.kdl ~/.config/zellij/config.kdl
+	./install_file.sh zellij/config.kdl          ~/.config/zellij/config.kdl
+	./install_file.sh zellij/nightfox-themes.kdl ~/.config/zellij/themes/nightfox-themes.kdl
 
