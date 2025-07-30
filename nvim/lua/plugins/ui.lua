a764ba6d57ab792c07cde51596457fde192cd885
Tim <Heaton15@gmail.com>
2025-07-29 23:48:58 -0400

    1. Add chatgpt to a custom Snacks picker

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -11,6 +11,114 @@ return {
         },
     },
     {
+        "robitx/gp.nvim",
+        config = function()
+            vim.keymap.set({ "n", "v" }, "<C-4>", function()
+                local Snacks = require("snacks")
+                local commands = {
+                    { name = "New Chat",        cmd = "GpChatNew popup" },
+                    { name = "Toggle Chat",     cmd = "GpChatToggle popup" },
+                    { name = "Find Chat",       cmd = "GpChatFinder" },
+                    { name = "Replace Chat",    cmd = "GpRewrite" },
+                    { name = "Append Chat",     cmd = "GpAppend" },
+                    { name = "Prepend Chat",    cmd = "GpPrepend" },
+                    { name = "New Buffer Chat", cmd = "GpEnew" },
+                    { name = "New Tab Chat",    cmd = "GpTabnew" },
+                    { name = "New Popup Chat",  cmd = "GpPopup" },
+                }
+
+                local items = {}
+                local longest_name = 0
+                for i, entry in ipairs(commands) do
+                    table.insert(items, {
+                        idx = i,
+                        score = i,
+                        name = entry.name,
+                        text = entry.cmd,
+                    })
+                    longest_name = math.max(longest_name, #entry.name)
+                end
+                longest_name = longest_name + 2
+
+                return Snacks.picker({
+                    items = items,
+                    format = function(item)
+                        return {
+                            { ('%-' .. longest_name .. 's'):format(item.name), 'SnacksPickerLabel' },
+                            { item.text,                                       'SnacksPickerComment' },
+                        }
+                    end,
+                    confirm = function(picker, item)
+                        picker:close()
+                        vim.cmd(item.text)
+                    end,
+                })
+            end, { desc = "Open ChatGPT picker" })
+            local function keymapOptions(desc)
+                return {
+                    noremap = true,
+                    silent = true,
+                    nowait = true,
+                    desc = "GPT prompt " .. desc,
+                }
+            end
+
+            -- Chat commands
+            vim.keymap.set("v", "<C-g>p", ":<C-u>'<,'>GpChatPaste popup<cr>", keymapOptions("Visual Chat Paste"))
+            vim.keymap.set("v", "<C-g>t", ":<C-u>'<,'>GpChatToggle<cr>", keymapOptions("Visual Toggle Chat"))
+            vim.keymap.set({ "n", "i" }, "<C-g>t", "<cmd>GpChatToggle<cr>", keymapOptions("Toggle Chat"))
+            vim.keymap.set({ "n", "i" }, "<C-g><C-v>", "<cmd>GpChatNew vsplit<cr>", keymapOptions("New Chat vsplit"))
+            vim.keymap.set("v", "<C-g><C-v>", ":<C-u>'<,'>GpChatNew vsplit<cr>", keymapOptions("Visual Chat New vsplit"))
+
+            -- Rewriting Prompts
+            vim.keymap.set({ "n", "i" }, "<C-g>r", "<cmd>GpRewrite<cr>", keymapOptions("Inline Rewrite"))
+            vim.keymap.set({ "n", "i" }, "<C-g>a", "<cmd>GpAppend<cr>", keymapOptions("Append (after)"))
+            vim.keymap.set({ "n", "i" }, "<C-g>b", "<cmd>GpPrepend<cr>", keymapOptions("Prepend (before)"))
+            vim.keymap.set("v", "<C-g>r", ":<C-u>'<,'>GpRewrite<cr>", keymapOptions("Visual Rewrite"))
+            vim.keymap.set("v", "<C-g>a", ":<C-u>'<,'>GpAppend<cr>", keymapOptions("Visual Append (after)"))
+            vim.keymap.set("v", "<C-g>b", ":<C-u>'<,'>GpPrepend<cr>", keymapOptions("Visual Prepend (before)"))
+
+            vim.keymap.set("v", "<C-g><C-x>", ":<C-u>'<,'>GpChatNew split<cr>", keymapOptions("Visual Chat New split"))
+            vim.keymap.set("v", "<C-g><C-t>", ":<C-u>'<,'>GpChatNew tabnew<cr>", keymapOptions("Visual Chat New tabnew"))
+            vim.keymap.set("v", "<C-g>i", "<cmd>'<,'>GpImplement<CR>", keymapOptions("Implement selection"))
+
+            -- Popup Prompts
+            vim.keymap.set({ "n", "i" }, "<C-g>gp", "<cmd>GpPopup<cr>", keymapOptions("Popup"))
+            vim.keymap.set({ "n", "i" }, "<C-g>ge", "<cmd>GpEnew<cr>", keymapOptions("GpEnew"))
+            vim.keymap.set({ "n", "i" }, "<C-g>gn", "<cmd>GpNew<cr>", keymapOptions("GpNew"))
+            vim.keymap.set({ "n", "i" }, "<C-g>gv", "<cmd>GpVnew<cr>", keymapOptions("GpVnew"))
+            vim.keymap.set({ "n", "i" }, "<C-g>gt", "<cmd>GpTabnew<cr>", keymapOptions("GpTabnew"))
+            vim.keymap.set("v", "<C-g>gp", ":<C-u>'<,'>GpPopup<cr>", keymapOptions("Visual Popup"))
+            vim.keymap.set("v", "<C-g>ge", ":<C-u>'<,'>GpEnew<cr>", keymapOptions("Visual GpEnew"))
+            vim.keymap.set("v", "<C-g>gn", ":<C-u>'<,'>GpNew<cr>", keymapOptions("Visual GpNew"))
+            vim.keymap.set("v", "<C-g>gv", ":<C-u>'<,'>GpVnew<cr>", keymapOptions("Visual GpVnew"))
+            vim.keymap.set("v", "<C-g>gt", ":<C-u>'<,'>GpTabnew<cr>", keymapOptions("Visual GpTabnew"))
+
+            local config = {
+
+                providers = {
+                    openai = {
+                        endpoint = "https://api.openai.com/v1/chat/completions",
+                        secret = os.getenv("OPENAI_API_KEY"),
+                    },
+                },
+                agents = {
+                    {
+                        name = "ChatGPT3-5",
+                        disable = true,
+                    },
+                },
+
+
+                openai_api_key = os.getenv("OPENAI_API_KEY")
+                -- For customization, refer to Install > Configuration in the Documentation/Readme
+            }
+            require("gp").setup(config)
+
+            -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+        end,
+    },
+    {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
