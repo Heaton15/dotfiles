174619dfec2b5170a224ff104110d2a9da0016be
Tim <Heaton15@gmail.com>
2025-11-04 10:48:20 -0600

    Force an install of rust software when the envvar FORCE=1 is set (or anything that is non-empty)

--- rust/install.mk
+++ rust/install.mk
@@ -3,8 +3,10 @@ ALL_TARGETS += rust
 
 .PHONY: rust
 
+FORCE ?=
+
 $(RUST_PACKAGES):
-	cargo install --root "$$HOME/.local/" --locked $@
+	cargo install --root "$$HOME/.local/" --locked $@ $(if $(FORCE),--force)
 
 rust: $(RUST_PACKAGES)
 
