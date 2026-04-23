77afeea1ec7511f5e512c4c2a3da43664d09391c
Tim Heaton <heaton15@gmail.com>
2026-04-23 14:23:10 -0400

    Grab the string filename

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,5 @@ require("config.verilog_autos")
 local Path = require("plenary.path")
 local work_lua = Path:new(vim.fn.expand("$HOME/.work.lua"))
 if work_lua:exists() then
-    vim.cmd.source(work_lua)
+    vim.cmd.source(work_lua.filename)
 end
