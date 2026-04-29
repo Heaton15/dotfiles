d0468a7039f0d2693533464f1d6071b76c5d5b0f
Tim Heaton <heaton15@gmail.com>
2026-04-29 12:45:13 -0400

    Add a utility template for managing the keyring

---
+++ scripts/work_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash                               
+                                                  
+add_key() {                                       
+  local keyName="$1"                              
+  if [[ -f "$HOME/.ssh/$keyName" ]]; then         
+    ssh-add "$HOME/.ssh/$keyName" > /dev/null 2>&1
+  fi                                              
+}                                                 
+                                                  
+key-work() {                                  
+    eval "$(ssh-agent -s)" > /dev/null 2>&1       
+    add_key ""
+}                                                 
+
+key-dotfiles() {                                  
+    eval "$(ssh-agent -s)" > /dev/null 2>&1       
+    add_key ""                          
+}                                                 
+                                                  
+key-work
