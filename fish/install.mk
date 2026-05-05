3d1a4e974e0686c803249420d0ad5e650026b429
Tim Heaton <heaton15@gmail.com>
2026-05-05 14:20:27 -0400

    Add fisher deps

--- fish/install.mk
+++ fish/install.mk
@@ -1,5 +1,10 @@
 .PHONY: fish
 ALL_TARGETS += fish
 
+# We can't install fisher deps from the CLI in a Makefile since
+# its not a bin and is instead a shell file.
+# Put the deps here so we can reference them later
+FISHER_DEPS := jorgebucaran/hydro \
+			   edc/bass
 fish:
 	./install_file.sh fish/config.fish ~/.config/fish/config.fish
