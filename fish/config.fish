c607d695d6724e5ec621b19804734811e0443eaf
Tim Heaton <heaton15@gmail.com>
2026-05-05 12:35:50 -0400

    Add the carbonfox highlighting for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,44 @@
 # Toss shell configuration inside of the is-interactive section
+function carbonfox_colors
+    # Nightfox Color Palette
+    # Style: carbonfox
+    # Upstream: https://github.com/edeneast/nightfox.nvim/raw/main/extra/carbonfox/carbonfox.fish
+    set -l foreground f2f4f8
+    set -l selection 2a2a2a
+    set -l comment 6e6f70
+    set -l red ee5396
+    set -l orange 3ddbd9
+    set -l yellow 08bdba
+    set -l green 25be6a
+    set -l purple be95ff
+    set -l cyan 33b1ff
+    set -l pink ff7eb6
+
+    # Syntax Highlighting Colors
+    set -g fish_color_normal $foreground
+    set -g fish_color_command $cyan
+    set -g fish_color_keyword $pink
+    set -g fish_color_quote $yellow
+    set -g fish_color_redirection $foreground
+    set -g fish_color_end $orange
+    set -g fish_color_error $red
+    set -g fish_color_param $purple
+    set -g fish_color_comment $comment
+    set -g fish_color_selection --background=$selection
+    set -g fish_color_search_match --background=$selection
+    set -g fish_color_operator $green
+    set -g fish_color_escape $pink
+    set -g fish_color_autosuggestion $comment
+
+    # Completion Pager Colors
+    set -g fish_pager_color_progress $comment
+    set -g fish_pager_color_prefix $cyan
+    set -g fish_pager_color_completion $foreground
+    set -g fish_pager_color_description $comment
+end
+
 if status is-interactive
+    carbonfox_colors
     function fish_mode_prompt
         switch $fish_bind_mode
             case default
@@ -13,7 +52,7 @@ if status is-interactive
         end
     end
     fish_vi_key_bindings
-    #enable_transience
+
     # Ghostty configuration 
     if test -n "$GHOSTTY_RESOURCES_DIR"
         builtin source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
@@ -30,7 +69,6 @@ if status is-interactive
         fish_add_path -a "/opt/homebrew/opt/scala@2.13/bin:$PATH"
         fish_add_path "/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
         set -gx RISCV /opt/homebrew/
-        #set -gx SHELL "/opt/homebrew/bin/bash"
     end
 
     if test -n WSLENV
@@ -81,9 +119,4 @@ if status is-interactive
 
     fish_add_path "$HOME/.local/bin"
     set -gx TMUX_TMPDIR "$HOME/.tmux"
-
-    if type -q starship
-        starship init fish | source
-        enable_transience
-    end
 end
