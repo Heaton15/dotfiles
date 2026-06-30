940169bbcac734cd129092d2ef10c5d72c082e7a
Tim Heaton <heaton15@gmail.com>
2026-06-30 10:37:34 -0400

    Move the sdk install into the build target as well

--- scala/install.mk
+++ scala/install.mk
@@ -2,19 +2,19 @@
 ALL_TARGETS += scala
 
 OS_TYPE := $(shell uname)
-
-ifeq ($(SDKMAN_DIR),)
-$(error Please install sdkman: >>> \
-	curl -s "https://get.sdkman.io?rcupdate=false" | bash \
-	<<<)
-endif
-
 DARWIN_ARM_CS := https://github.com/coursier/launchers/blob/master/cs-aarch64-apple-darwin.gz
 DARWIN_x86_64_CS := https://github.com/coursier/launchers/blob/master/cs-x86_64-apple-darwin.gz 
 LINUX_x86_64_CS  := https://github.com/coursier/launchers/blob/master/cs-x86_64-pc-linux.gz
 JAR_LAUNCHER_CS := https://github.com/coursier/launchers/raw/master/coursier
 
 scala:
+	# Need to have sdk to manage JVM for scala
+	@if ! command -v sdk > /dev/null 2>&1; then \
+		echo "Please install sdkman: >>> curl -s https://get.sdkman.io?rcupdate=false | bash  <<<"; \
+		exit 1; \
+	fi
+
+	# If we don't have coursier, give the install user a install version
 	@if ! command -v cs > /dev/null 2>&1; then \
 		if [ "$(OS_TYPE)" = "Darwin" ] && [ "$(HOSTTYPE)" = "aarch64" ]; then \
 			echo "Please install coursier: >>> curl -fL ${DARWIN_ARM_CS} | gunzip -c > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs <<<"; \
@@ -27,6 +27,7 @@ scala:
 			exit 1; \
 		fi; \
 	fi
+
 	cs update --quiet
 	./install_file.sh scala/scalafix.conf ~/.scalafix.conf
 	./install_file.sh scala/scalafmt.conf ~/.scalafmt.conf
