febba2f28c58b52059c358bc36f51f9f3069cbee
Tim Heaton <heaton15@gmail.com>
2026-06-30 10:43:55 -0400

    Fix the conditionals on the installs

--- scala/install.mk
+++ scala/install.mk
@@ -9,13 +9,13 @@ JAR_LAUNCHER_CS := https://github.com/coursier/launchers/raw/master/coursier
 
 scala:
 	# Need to have sdk to manage JVM for scala
-	@if ! command -v sdk > /dev/null 2>&1; then \
+	@if command -v sdk > /dev/null 2>&1; then \
 		echo "Please install sdkman: >>> curl -s https://get.sdkman.io?rcupdate=false | bash  <<<"; \
 		exit 1; \
 	fi
 
 	# If we don't have coursier, give the install user a install version
-	@if ! command -v cs > /dev/null 2>&1; then \
+	@if command -v cs > /dev/null 2>&1; then \
 		if [ "$(OS_TYPE)" = "Darwin" ] && [ "$(HOSTTYPE)" = "aarch64" ]; then \
 			echo "Please install coursier: >>> curl -fL ${DARWIN_ARM_CS} | gunzip -c > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs <<<"; \
 			exit 1; \
@@ -24,6 +24,9 @@ scala:
 			exit 1; \
 		elif [ "$(OS_TYPE)" = "Linux" ] && [ "$(HOSTTYPE)" = "x86_64" ]; then \
 			echo "Please install coursier: >>> curl -fL ${LINUX_x86_64_CS} > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs <<<"; \
+			exit 1; \
+		else \
+			echo "Host Machine is neither Mac or Linux..."; \
 			exit 1; \
 		fi; \
 	fi
