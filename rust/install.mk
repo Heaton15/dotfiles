9f032ea1a014641499a1d8c6036bee9e9a605956
Tim Heaton <heaton15@gmail.com>
2026-06-30 10:27:14 -0400

    Move the coursier and cargo checking into their respective scala / rust targets to prevent other targets from failing to build when specifically invoked

--- rust/install.mk
+++ rust/install.mk
@@ -3,7 +3,13 @@ ALL_TARGETS += rust
 
 FORCE ?=
 
-$(RUST_PACKAGES):
+cargo_install:
+	@if ! command -v cargo > /dev/null 2>&1; then \
+		echo "Please install cargo: >>> curl https://sh.rustup.rs -sSf | sh <<<"; \
+		exit 1; \
+	fi
+
+$(RUST_PACKAGES): cargo_install
 	cargo install --root "$$HOME/.local/" --locked $@ $(if $(FORCE),--force)
 
 rust: $(RUST_PACKAGES)
--- scala/install.mk
+++ scala/install.mk
@@ -1,7 +1,7 @@
 .PHONY: scala
 ALL_TARGETS += scala
 
-OS_TYPE := $(shell uname -a | cut -d' ' -f1)
+OS_TYPE := $(shell uname)
 
 ifeq ($(SDKMAN_DIR),)
 $(error Please install sdkman: >>> \
@@ -11,33 +11,22 @@ endif
 
 DARWIN_ARM_CS := https://github.com/coursier/launchers/blob/master/cs-aarch64-apple-darwin.gz
 DARWIN_x86_64_CS := https://github.com/coursier/launchers/blob/master/cs-x86_64-apple-darwin.gz 
-LINUX_x86_64_CS := https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux
+LINUX_x86_64_CS  := https://github.com/coursier/launchers/blob/master/cs-x86_64-pc-linux.gz
 JAR_LAUNCHER_CS := https://github.com/coursier/launchers/raw/master/coursier
 
-ifeq ($(shell which cs 2> /dev/null),)
-	ifeq ($(OS_TYPE), Darwin)
-		ifeq ($(HOSTTYPE), aarch64)
-        $(error Please install coursier: >>> \
-            curl -fL ${DARWIN_ARM_CS} | gunzip -c > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs \
-		 <<<)
-		endif
-		ifeq ($(HOSTTYPE), x86_64)
-        $(error Please install coursier: >>> \
-            curl -fL ${DARWIN_x86_64_CS} | gunzip -c > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs \
-		 <<<)
-		endif
-	endif
-
-	ifeq ($(OS_TYPE), Linux)
-		ifeq ($(HOSTTYPE), x86_64)
-        $(error Please install coursier: >>> \
-            curl -fL ${LINUX_x86_64_CS} > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs \
-		 <<<)
-		endif
-	endif
-endif
-
 scala:
+	@if ! command -v cs > /dev/null 2>&1; then \
+		if [ "$(OS_TYPE)" = "Darwin" ] && [ "$(HOSTTYPE)" = "aarch64" ]; then \
+			echo "Please install coursier: >>> curl -fL ${DARWIN_ARM_CS} | gunzip -c > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs <<<"; \
+			exit 1; \
+		elif [ "$(OS_TYPE)" = "Darwin" ] && [ "$(HOSTTYPE)" = "x86_64" ]; then \
+			echo "Please install coursier: >>> curl -fL ${DARWIN_x86_64_CS} | gunzip -c > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs <<<"; \
+			exit 1; \
+		elif [ "$(OS_TYPE)" = "Linux" ] && [ "$(HOSTTYPE)" = "x86_64" ]; then \
+			echo "Please install coursier: >>> curl -fL ${LINUX_x86_64_CS} > ~/.local/bin/cs && chmod 755 ~/.local/bin/cs <<<"; \
+			exit 1; \
+		fi; \
+	fi
 	cs update --quiet
 	./install_file.sh scala/scalafix.conf ~/.scalafix.conf
 	./install_file.sh scala/scalafmt.conf ~/.scalafmt.conf
